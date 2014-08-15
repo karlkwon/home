b22abab8a252c8f43492fd6fb581d755df15a261
mrthink <mrthink@localhost>
2014-08-15 23:45:38 +0900

    basic test code...

--- jsoupTest/src/main/java/com/karlkwon/home/jsoupTest/App.java
+++ jsoupTest/src/main/java/com/karlkwon/home/jsoupTest/App.java
@@ -1,13 +1,36 @@
 package com.karlkwon.home.jsoupTest;
 
-/**
- * Hello world!
- *
- */
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
+//    	String		url		= "http://www.insight.co.kr/";
+//    	try {
+//			Document	doc		= Jsoup.connect(url).get();
+//			Elements		title	= doc.select(".title");
+//			
+//			for(Element e: title) {
+//				System.out.println("text: " + e.text());
+//				System.out.println("text: " + e.html());
+//			}
+//
+//			Elements		links	= doc.select("a[href]");
+//			for(Element l: links) {
+//				System.out.println("text: " + l.attr("abs:href"));
+//			}
+//			
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+    	
         System.out.println( "Hello World!" );
     }
 }
